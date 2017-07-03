05fc3fa1f681e13a49e63c744d1ff3d9ee44417d
Vladimir Skabelkin <v.skabelkin@gmail.com>
2017-07-03 16:20:35 +0300

    Add files via upload

---
+++ ch03 - Decision tree.py
@@ -0,0 +1,168 @@
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import export_graphviz
+import pydotplus
+
+def versiontuple(v):
+    return tuple(map(int, (v.split("."))))
+
+def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
+
+    # setup marker generator and color map
+    markers = ('s', 'x', 'o', '^', 'v')
+    colors = ('red', 'blue', 'lightgreen', 'gray', 'cyan')
+    cmap = ListedColormap(colors[:len(np.unique(y))])
+
+    # plot the decision surface
+    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution),
+                           np.arange(x2_min, x2_max, resolution))
+    Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
+    Z = Z.reshape(xx1.shape)
+    plt.contourf(xx1, xx2, Z, alpha=0.4, cmap=cmap)
+    plt.xlim(xx1.min(), xx1.max())
+    plt.ylim(xx2.min(), xx2.max())
+
+    for idx, cl in enumerate(np.unique(y)):
+        plt.scatter(x=X[y == cl, 0],
+                    y=X[y == cl, 1],
+                    alpha=0.6,
+                    c=cmap(idx),
+                    edgecolor='black',
+                    marker=markers[idx],
+                    label=cl)
+
+    # highlight test samples
+    if test_idx:
+        # plot all samples
+        if not versiontuple(np.__version__) >= versiontuple('1.9.0'):
+            X_test, y_test = X[list(test_idx), :], y[list(test_idx)]
+            warnings.warn('Please update to NumPy 1.9.0 or newer')
+        else:
+            X_test, y_test = X[test_idx, :], y[test_idx]
+
+        plt.scatter(X_test[:, 0],
+                    X_test[:, 1],
+                    c='',
+                    alpha=1.0,
+                    edgecolor='black',
+                    linewidths=1,
+                    marker='o',
+                    s=55, label='test set')
+
+def gini(p):
+    return p * (1 - p) + (1 - p) * (1 - (1 - p))
+
+
+def entropy(p):
+    return - p * np.log2(p) - (1 - p) * np.log2((1 - p))
+
+
+def error(p):
+    return 1 - np.max([p, 1 - p])
+
+x = np.arange(0.0, 1.0, 0.01)
+
+ent = [entropy(p) if p != 0 else None for p in x]
+sc_ent = [e * 0.5 if e else None for e in ent]
+err = [error(i) for i in x]
+
+fig = plt.figure()
+ax = plt.subplot(111)
+for i, lab, ls, c, in zip([ent, sc_ent, gini(x), err],
+                          ['Entropy', 'Entropy (scaled)',
+                           'Gini Impurity', 'Misclassification Error'],
+                          ['-', '-', '--', '-.'],
+                          ['black', 'lightgray', 'red', 'green', 'cyan']):
+    line = ax.plot(x, i, label=lab, linestyle=ls, lw=2, color=c)
+
+ax.legend(loc='upper center', bbox_to_anchor=(0.5, 1.15),
+          ncol=3, fancybox=True, shadow=False)
+
+ax.axhline(y=0.5, linewidth=1, color='k', linestyle='--')
+ax.axhline(y=1.0, linewidth=1, color='k', linestyle='--')
+plt.ylim([0, 1.1])
+plt.xlabel('p(i=1)')
+plt.ylabel('Impurity Index')
+plt.tight_layout()
+#plt.savefig('./figures/impurity.png', dpi=300, bbox_inches='tight')
+plt.show()
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 0)
+
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+
+tree = DecisionTreeClassifier(criterion='entropy', max_depth=3, random_state=0)
+tree.fit(X_train, y_train)
+
+X_combined = np.vstack((X_train, X_test))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X_combined, y_combined,
+                      classifier=tree, test_idx=range(105, 150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+# plt.savefig('./figures/decision_tree_decision.png', dpi=300)
+plt.show()
+
+export_graphviz(tree,
+                out_file='tree.dot',
+                feature_names=['petal length', 'petal width'])
+
+from IPython.display import Image
+from IPython.display import display
+
+try:
+
+    import pydotplus
+
+    dot_data = export_graphviz(
+        tree,
+        out_file=None,
+        # the parameters below are new in sklearn 0.18
+        feature_names=['petal length', 'petal width'],
+        class_names=['setosa', 'versicolor', 'virginica'],
+        filled=True,
+        rounded=True)
+
+    graph = pydotplus.graph_from_dot_data(dot_data)
+    display(Image(graph.create_png()))
+
+except ImportError:
+    print('pydotplus is not installed.')
+
+
+forest = RandomForestClassifier(criterion='entropy',
+                                n_estimators=10,
+                                random_state=1,
+                                n_jobs=2)
+forest.fit(X_train, y_train)
+
+plot_decision_regions(X_combined, y_combined,
+                      classifier=forest, test_idx=range(105, 150))
+
+plt.xlabel('petal length [cm]')
+plt.ylabel('petal width [cm]')
+plt.legend(loc='upper left')
+plt.tight_layout()
+# plt.savefig('./figures/random_forest.png', dpi=300)
+plt.show()
